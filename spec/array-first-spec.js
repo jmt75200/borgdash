881f0e4f295ff808e10fb6b1952e12f10e616da7
jmt75200 <joelle@pcgamerzhawaii.com>
2015-01-16 18:50:14 -1000

    adds array-spec js files, begins testing parameters for array-first-spec

---
+++ spec/array-first-spec.js
@@ -0,0 +1,33 @@
+var borgArray = require('../borgdash.js');
+
+describe("borgArray", function() {
+  
+
+  it("Must be a function; have function named first()", function() {
+
+    expect( borgArray.first ).toBeDefined();
+    
+  });
+
+  it("Must get the first element of the first array argument", function (){
+    var test_array = ["fish","dog","taku","panda","unicorn"];
+
+    var result = borgArray.first(test_array);
+
+    expect( result ).toBe("fish");
+
+  });
+
+  it("Returns the n elements of the first array argument", function() {
+    var test_array = ["fish","dog","taku","panda","unicorn"];
+
+    var result2 = borgArray.first(test_array, 2);
+    expect( result2 ).toEqual( ["fish","dog"] );
+
+    var result3 = borgArray.first(test_array, 4);
+    expect( result3 ).toEqual( ["fish","dog","taku","panda","unicorn"] );
+    
+  });
+
+
+});
